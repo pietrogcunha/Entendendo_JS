1b77e01256c559ff02186aa5dbd789ef5cdb7e83
pietrogcunha <pietro.grgcunha@senacsp.edu.br>
2024-08-23 21:04:45 -0300

    feat: Código base do repositório

---
+++ ex1.js
@@ -0,0 +1,16 @@
+let nota1,nota2 = 0;
+let nome = ""
+ 
+let nome1 = prompt("Digite o Nome do Aluno 1:");
+let nota1 = parseFloat(prompt("Digite a Nota 1 do Aluno 1"));
+let nota2 = parseFloat(prompt("Digite a Nota 2 do Aluno 1"));
+ 
+var media1 = 0;
+media1 = (nota1 + nota2) / 2;
+ 
+let numero = media1
+if (numero 7 6) {
+    ("Aprovado");
+} else if (numero <4) {
+    ("Reprovado");
+}
