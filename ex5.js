7cc9bb391198e1ce396ef1a045394ef4c2ba299b
pietrogcunha <pietro.grgcunha@senacsp.edu.br>
2024-08-23 21:53:30 -0300

    feat: exercícios de fixação

--- ex5.js
+++ ex5.js
@@ -1,1 +1,30 @@
-// Faça um algoritmo que 
+let num1 = parseInt(prompt("Digite o primeiro numero: "));
+let num2 = parseInt(prompt("Digite o segundo numero: "));
+let num3 = parseInt(prompt("Digite o terceiro numero: "));
+let maior, menor;
+
+//BLOCO DO MAIOR
+if((num1 >= num2) && (num1 >= num3)){
+maior = num1;
+}
+else if ((num2 >= num1) && (num2 >= num3)){
+maior = num2;
+}
+else if ((num3 >= num1) && (num3 >= num2)){
+    maior = num3;
+}
+
+//BLOCO DO MENOR
+if((num1 <= num2) && (num1 <= num3)){
+    menor = num1;
+    }
+    else if ((num2 <= num1) && (num2 <= num3)){
+    menor = num2;
+    }
+    else if ((num3 <= num1) && (num3 <= num2)){
+    menor = num3;
+    }
+        
+    alert(`O numero ${maior} é o maior de todos!
+        E o ${menor} é o menor de todos!`);
+        
---
+++ ex6.js
@@ -0,0 +1,6 @@
+/*faça um algoritimo que pergunte o nume do usuario, o quanto ganha por hora e
+numero de horas trabalhadas no mes, calcule o resultado e mostre o nome e o
+total de renda recebida no mês.*/
+
+let nome = prompt("Informe seu nome")
+let 
